7a81af61180c07d3f88bd6c09bf8030c7c5a2f38
Michaela Aranas <ella4aranas@gmail.com>
2016-09-08 20:43:32 +0800

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,50 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+* SOURCE CODE: ArrayRandom.java
+* AUTHOR NAME: Aranas, Michaela M.
+* GITHUB URL: [github.com/michaela4]
+* LAST UPDATE: [09/07/2016]
+*
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+*/
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class LinearSearch {
+  private int array [];
+  private int capacity;
+  
+  public LinearSearch(int size) {
+     capacity = size;
+     array = new int[capacity];
+   }
+  
+  public void toSearch(int search){
+    
+    for (int i = 0; i < capacity; i++){
+      if ( array [i] == search ){
+        System.out.println("FOUND: " + search + " is at array["+ i +"]");
+          break;
+      }
+      if (i == capacity){}
+      System.out.println("NOT FOUND: " + search + " is not in the array.");
+      break;
+    }
+  }
+  
+  public static void main (String args[]){
+    Scanner input = new Scanner (System.in);
+    
+    System.out.print ("Enter array size: ");
+    int size =  input.nextInt();
+    LinearSearch implement = new LinearSearch(size);
+    for (int k = 0; k < size; k++){
+       Random rand = new Random();
+       implement.array[k] = rand.nextInt(1000);
+    }
+    
+    System.out.print ("Enter number to find: ");
+    int search = input.nextInt();
+    implement.toSearch(search); 
+  }
+}
